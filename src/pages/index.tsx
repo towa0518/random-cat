1009a539b3d4f048fc05766e7531f1a3c7623433
towa0518 <sakasyu.10.22@outlook.com>
2023-02-06 07:55:30 +0900

    3つの猫の画像のうちランダムで1つ表示させる

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const catImages: string[] = [
+  "https://cdn2.thecatapi.com/images/bpc.jpg",
+  "https://cdn2.thecatapi.com/images/eac.jpg",
+  "https://cdn2.thecatapi.com/images/6qi.jpg",
+];
+
+const randomCatImage = (): string => {
+  const index = Math.floor(Math.random() * catImages.length);
+  return catImages[index];
+};
+
+const IndexPage = () => {
+  const [catImageUrl, setCatImageUrl] = useState(
+    "https://cdn2.thecatapi.com/images/bpc.jpg"
+  );
+
+  const handleClick = () => {
+    setCatImageUrl(randomCatImage());
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
